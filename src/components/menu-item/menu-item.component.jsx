5c84aa4c54e16e69a911f44db214c0afc8809133
Yonatan Brauner <yonabrauner@gmail.com>
2025-04-28 18:58:25 +0300

    broke homepage into compnents and styled

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+export const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`menu-item ${size}`}>
+        <div className='background-image' style={{ 
+            backgroundImage: `url("${imageUrl}")`
+            }}
+        />
+        <div className='content'>
+            <h1 className='title open-sans'>{title.toUpperCase()}</h1>
+            <span className='subtitle open-sans'>SHOP NOW</span>
+        </div>
+    </div>
+)
