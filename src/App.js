0948a5d850de03a748e50ead280edbeb2924d69b
Yonatan Brauner <yonabrauner@gmail.com>
2025-05-05 18:46:26 +0300

    continued sign in components and styles.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HomePage } from './pages/homepage/homepage.component';
 import { Route, Routes } from 'react-router-dom';
 import { ShopPage } from './pages/shop/shop.component';
 import { Header } from './components/header/header.component';
+import { SignInAndSignUp } from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage/>}/>
         <Route exact path="/shop" element={<ShopPage/>}/>
+        <Route exact path="/sign-in" element={<SignInAndSignUp/>}/>
       </Routes>
     </div>
   );
